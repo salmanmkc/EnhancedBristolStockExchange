8c667d6049d5a6738fc5fa51a1a3066a50bd2c8b
SalmanMKC <13schishti@gmail.com>
2022-05-09 14:22:42 +0100

    how does performance vary when we change the number of agents

--- processResults.py
+++ processResults.py
@@ -8,7 +8,7 @@ avgBalance = pd.read_csv("avg_balance.csv",header=None,\
                          names=list(string.ascii_uppercase)[0:22],\
                          index_col=False)
 
-newNames = { "H":"SNPR", "L":"SHVR", "P":"ZIC", "T":"ZIP"}
+newNames = { "H":"GVWY", "L":"SHVR", "P":"ZIC", "T":"ZIP"}
 avgBalance.rename(columns=newNames,inplace=True)
-avgBalance.plot(x="B",y=["SNPR","SHVR","ZIC","ZIP"], kind="line")
+avgBalance.plot(x="B",y=["GVWY","SHVR","ZIC","ZIP"], kind="line")
 plt.show(block=True)
